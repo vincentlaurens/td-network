da565a601480d54e31431ca5594a00a8a98c68b0
Vincent.Laurens <Laurensvincent31@gmail.com>
2017-11-27 17:13:24 +0100

    UDP et TCP Valide

--- src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageReceiverService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageReceiverService.java
@@ -1,8 +1,37 @@
 package com.fredericboisguerin.insa.network.core.service;
 
-public class TCPMessageReceiverService implements MessageReceiverService {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPMessageReceiverService implements MessageReceiverService, Runnable {
+    private ServerSocket serverSocket;
+    private BufferedReader reader;
+    private InputStreamReader stream;
+    private IncomingMessageListener monIncomingMessageListener;
+
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+        this.serverSocket = new ServerSocket(port);
+        this.monIncomingMessageListener = incomingMessageListener;
+        Socket chatSocket = serverSocket.accept();
+
+
+        this.stream = new InputStreamReader(chatSocket.getInputStream());
+        this.reader  =new BufferedReader(this.stream);
+        new Thread(()-> this.run()).start();
+    }
+    public void run(){
+        try{
+            boolean threadteceptionactive = true;
+            while (threadteceptionactive){
+                String message = this.reader.readLine();
+                this.monIncomingMessageListener.onNewIncomingMessage(message);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageSenderService.java
+++ src/main/java/com/fredericboisguerin/insa/network/core/service/TCPMessageSenderService.java
@@ -1,8 +1,19 @@
 package com.fredericboisguerin.insa.network.core.service;
 
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class TCPMessageSenderService implements MessageSenderService {
+    private Socket chatSocket;
+    private PrintWriter writer;
+
     @Override
     public void sendMessageOn(String ipAddress, int port, String message) throws Exception {
-
+        chatSocket = new Socket(ipAddress,port);
+        
+        writer = new PrintWriter(chatSocket.getOutputStream());
+        writer.println(message);
+        writer.close();
     }
 }
